9e8a7487e553001ad90495b94f0f6cad0e55e7fc
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-01 12:26:35 +0530

    Create arithematic.java

---
+++ arithematic.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Arithematic
+{
+	public static void main(String args[])
+	{
+		int a;
+		int b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the values of a,b: ");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println("add is: "+(a+b));
+		System.out.println("sub is: "+(a-b));
+		System.out.println("muti is: "+(a*b));
+		System.out.println("div is: "+(a/b));
+		System.out.println("mod is: "+(a%b));
+	}
+}		
