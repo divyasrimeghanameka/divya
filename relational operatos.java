07084090213cb73491e559c5e07973227799f69c
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-01 12:16:37 +0530

    Create relational operatos.java

---
+++ relational operatos.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Rop
+{
+  public static void main(String[]args)
+  {
+   int a,b;
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter a,b values:");
+   a=sc.nextInt();
+   b=sc.nextInt();
+   System.out.println("a>b:"+(a>b));
+   System.out.println("a<b:"+(a<b));
+   System.out.println("a<=b:"+(a<=b));
+   System.out.println("a>=b:"+(a>=b));
+   System.out.println("a==b:"+(a==b));
+   System.out.println("a!=b:"+(a!=b));
+  }
+}
+   
