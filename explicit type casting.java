9a8dcb8166da7192401958f584dc35e170d480cd
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-08 11:55:19 +0530

    Create explicit type casting.java

---
+++ explicit type casting.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Narrow
+{
+  public static void main(String args[])
+  { 
+     double d=10.69;
+     System.out.println("double value is: "+d);
+     int num=(int)d;
+     System.out.println("integer value is: "+num);
+     byte b=(byte)num;
+     System.out.println("byte value is: "+b);
+     
+         }
+}
+   
