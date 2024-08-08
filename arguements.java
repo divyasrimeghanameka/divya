05b46b32962cf4fee13c14d38e7d9ae7c0a3a317
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-08 11:12:29 +0530

    Create arguements.java

---
+++ arguements.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Cla
+{
+  public static void main(String args[])
+  {
+    System.out.println("the first arguement : "+args[0]);
+    System.out.println("the second arguement : "+args[1]);
+   }
+ }
