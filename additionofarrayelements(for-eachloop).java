d49d519dd8967801edfc6aa78884e5cd7c2b8c45
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-08 11:26:19 +0530

    Create additionofarrayelements(for-eachloop).java

---
+++ additionofarrayelements(for-eachloop).java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Add
+{
+  public static void main(String args[])
+  {
+     int arr[]={10,11,12,13};
+     int sum=0;
+     for(int i:arr)
+     {
+       sum+=i;
+     }
+     System.out.println("sum of array elements: "+sum);
+  }
+}
+   
