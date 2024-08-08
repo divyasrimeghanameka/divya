3a56decf82fda4a90826d4f4e543ae0bd3a4af03
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-08 11:46:51 +0530

    Create implicit type casting.java

---
+++ implicit type casting.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Wide
+{
+  public static void main(String args[])
+  { 
+     int num=10;
+     System.out.println("integer value is: "+num);
+     double d=num;
+     System.out.println("double value is: "+d);
+     
+    
+  }
+}
+   
