90ff3a91c664e261815e6555a68b32f07d41f312
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-08 12:14:16 +0530

    Create check num is pos or neg.java

---
+++ check num is pos or neg.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Num
+{
+  public static void main(String args[])
+  { 
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter a number: ");
+     int a=sc.nextInt();
+     if(a>0)
+     {
+       System.out.println(a+" is positive");
+    
+     }
+     else 
+     {
+        System.out.println(a+" is negative");
+      }
+         }
+}
+   
