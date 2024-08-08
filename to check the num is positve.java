d94072cb0854763450e5460a935cb16212437300
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-08 12:12:06 +0530

    Create to check the num is positve.java

---
+++ to check the num is positve.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Num
+{
+  public static void main(String args[])
+  { 
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter a number: ");
+     int a=sc.nextInt();
+     if(a>0)
+     {
+       System.out.println(a+" is positive");
+    
+     }
+         }
+}
+   
