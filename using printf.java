c988a2a9c4c38bc2be423336d1f96d6e112998d7
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-08 12:23:11 +0530

    Create using printf.java

---
+++ using printf.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Spec
+{
+  public static void main(String args[])
+  { 
+     int age=20;
+     String name="Divya";
+     double score=80.9;
+     System.out.printf("%d,%s,%f",age,name,score);
+     
+  }
+}
+     
