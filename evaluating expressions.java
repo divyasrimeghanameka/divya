ada7044dad204891dcaa8e06fc72836c149b7c35
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-08 12:01:13 +0530

    Create evaluating expressions.java

---
+++ evaluating expressions.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Assoc
+{
+  public static void main(String args[])
+  { 
+     int a,b;
+     a=12/3*4+2;
+     b=12*4*2/1+2;
+     System.out.println("the value of a: "+a);
+     System.out.println("the value of b: "+b);
+    
+     
+         }
+}
+   
