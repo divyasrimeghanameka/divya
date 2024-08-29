d641e6c6b5e9c1515596dc0f89657f7182a646b1
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-08-29 11:31:00 +0530

    Update largest of the 3.java

--- largest of the 3.java
+++ largest of the 3.java
@@ -9,20 +9,17 @@ class Largest
       a=sc.nextInt();
       b=sc.nextInt();
       c=sc.nextInt();
-      if(a>b)
+      if(a>b && a>c)
       {
-         if(a>c)
-         System.out.println(a+" is largest number.");
-         else
-         System.out.println(c+" is largest number.");
+        System.out.println(a+" is the largest number.");
+      }
+      else if(b>a && b>c)
+      {
+        System.out.println(b+" is the largest number.");
       }
       else
-      { 
-         if(b>a)
-         System.out.println(b+" is largest number.");
-         else
-         System.out.println(c+" is the largest number.");
-         
+      {
+        System.out.println(c+" is the largest number.");
       }
    }
 }
