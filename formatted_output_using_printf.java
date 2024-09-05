807e46c582322af23d13f5ec5e4dd7bd4bb38bfb
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-09-05 22:28:30 +0530

    Create formatted_output_using_printf.java

---
+++ formatted_output_using_printf.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ class format
+ {
+   public static void main(String args[])
+   { 
+    String name="rock";
+    int age=12;
+    String branch="CSE";
+    double perc=98.07;
+    System.out.printf("%s is %d years old studying %s %f",name,age,branch,perc);
+   }
+ }
