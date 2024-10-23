be640922771b695cbcb239e537f48929b5ece497
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-10-23 23:13:14 +0530

    Create textfiles.java

---
+++ textfiles.java
@@ -0,0 +1,30 @@
+writing text files
+import java.io.*;
+public class fos
+{
+    public static void main(String[]args)throws FileNotFoundException,IOException
+    {
+        FileOutputStream fos=new FileOutputStream("abc.txt");
+        fos.write(5);
+        System.out.println("sucess");
+        fos.flush();
+        fos.close();
+    }
+}
+output:success
+reading textfiles
+import java.io.*;
+public class fis
+{
+    public static void main(String[]args)throws FileNotFoundException,IOException
+    {
+        FileInputStream fis=new FileInputStream("bbc.txt");
+        fis.read();
+        System.out.println(fis.read());
+        fis.close();
+    }
+}
+output of the program if file bbc contain hi text
+ the output of the program will be:
+105
+
