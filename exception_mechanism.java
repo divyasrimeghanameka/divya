d29877ded795869af50065378aa91d925fe2ef47
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-10-23 14:19:59 +0530

    Create exception_mechanism.java

---
+++ exception_mechanism.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+class Except
+{
+    public static void main(String[]args)
+    {
+        try
+        {
+            int a[]={1,23,18,16,32};
+            System.out.println(a[7]);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            System.out.println("finally block executed");
+        }
+        try
+        {
+            validate(18);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+        public static void validate(int age)
+        {
+            if(age<18)
+            {
+                throw new ArithmeticException("not eligible");
+            }
+            else
+            {
+                System.out.println("eligible");
+            }
+        }
+}
+
