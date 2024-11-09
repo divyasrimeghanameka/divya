c9677aba4337959e6800a895847dfe11ec602fec
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-11-09 09:39:59 +0530

    Update textfiles.java

--- textfiles.java
+++ textfiles.java
@@ -1,30 +1,18 @@
-writing text files
 import java.io.*;
-public class fos
-{
-    public static void main(String[]args)throws FileNotFoundException,IOException
-    {
-        FileOutputStream fos=new FileOutputStream("abc.txt");
-        fos.write(5);
-        System.out.println("sucess");
+
+class Copy {
+    public static void main(String[] args) throws FileNotFoundException,IOException {
+        FileInputStream fis = new FileInputStream("abc.txt");
+        FileOutputStream fos = new FileOutputStream("bbc.txt");
+        int data;
+        while((data = fis.read()) != -1){
+            fos.write(data);
+        }
+        System.out.println("Copied");
+        fis.close();
         fos.flush();
         fos.close();
     }
 }
-output:success
-reading textfiles
-import java.io.*;
-public class fis
-{
-    public static void main(String[]args)throws FileNotFoundException,IOException
-    {
-        FileInputStream fis=new FileInputStream("bbc.txt");
-        fis.read();
-        System.out.println(fis.read());
-        fis.close();
-    }
-}
-output of the program if file bbc contain hi text
- the output of the program will be:
-105
-
+output:
+copied
